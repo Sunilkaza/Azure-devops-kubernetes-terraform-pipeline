aab8ddb1c4368e1686508eeb14f9ae334b6e0444
Sunilkaza <kazaparv@gmail.com>
2021-08-23 16:10:19 -0400

    Update main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -16,10 +16,9 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
       key_data = file(var.ssh_public_key)
     }
   }
-
-  provider "azurerm_resource_group" {
-  features {}
-  }
+provider "azurerm_resource_group" {
+ 
+}
   
   
   default_node_pool {
