e7cd5ff7bff78dcc601e8b92fa2fd1ab5dbcbbe1
Sunilkaza <kazaparv@gmail.com>
2021-08-24 22:57:57 -0400

    Update main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -16,39 +16,6 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
       key_data = file(var.ssh_public_key)
     }
   }
-
-  provider "azurerm" {
-  features {}
-  subscription_id = "d09feed3-4053-43d3-900a-cbb8a38db0a5"
-  skip_provider_registration = "true"
-}
-
-provider "azurerm" {
-  features {}
-  subscription_id = "d09feed3-4053-43d3-900a-cbb8a38db0a5"
-  skip_provider_registration = "true"
-  alias  = "shared"
-}
-
-module "cluster-addons" {
-  source = "./modules/cluster-addons"
-  providers = {
-    azurerm.shared = azurerm.shared
-    azurerm    = azurerm
-  }
-}
-
-data "azurerm_resource_group" "MCSACRrg" {
-  provider = azurerm.shared
-  name = var.MCSACRrg
-}
-
-resource "azurerm_role_assignment" "acrpull_role" {
-  scope                            = data.azurerm_resource_group.MCSACRrg.id
-  role_definition_name             = "AcrPull"
-  principal_id                     = data.azurerm_kubernetes_cluster.k8s.kubelet_identity.0.object_id
-  skip_service_principal_aad_check = true
-}
   
   default_node_pool {
     name            = "agentpool"
